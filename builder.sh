9d3f13e8baafa2b15119b5c9ae5402cdaa8b98a8
PsyMan47 <xhonirexhepi@gmail.com>
2017-10-04 17:23:05 +0200

    Add support for gemini

--- builder.sh
+++ builder.sh
@@ -19,6 +19,7 @@ while true; do
     "2" "MI5s Plus" \
     "3" "MI Mix" \
     "4" "MI Note 2" \
+    "4" "MI 5" \
     2>&1 1>&3)
   exit_status=$?
   exec 3>&-
@@ -205,6 +206,48 @@ cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/scorpio/
 cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/scorpio/
 cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/scorpio/
 cd $ANYKERNEL_DIR/scorpio
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+      ;;
+    5 )
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=../AnyKernel2
+DTBTOOL=$KERNEL_DIR/dtbTool
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Psychedelic-Kernel"
+DEVICE="-gemini-"
+VER="-v0.2"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$VER".zip
+
+rm -rf $ANYKERNEL_DIR/gemini/*.ko && rm $ANYKERNEL_DIR/gemini/zImage $ANYKERNEL_DIR/gemini/dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="Psy_Man"
+export KBUILD_BUILD_HOST="PsyBuntu"
+export CROSS_COMPILE=/home/psybuntu/toolchain/aarch64-linux-android-5.3/bin/aarch64-linux-android-
+export LD_LIBRARY_PATH=/home/psybuntu/toolchain/aarch64-linux-android-5.3/lib/
+export USE_CCACHE=1
+export CCACHE_DIR=/home/psybuntu/DragonXia-OC/CCACHES/gemini/.ccache
+
+make clean && make mrproper
+make gemini_defconfig
+make -j3
+
+./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
+cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/gemini/dtb
+cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/gemini/zImage
+cp $KERNEL_DIR/drivers/staging/qcacld-2.0/wlan.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/char/hw_random/msm_rng.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/char/hw_random/rng-core.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/char/rdbg.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/spi/spidev.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/input/evbug.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/crypto/ansi_cprng.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/mmc/card/mmc_test.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/video/backlight/lcd.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/video/backlight/backlight.ko $ANYKERNEL_DIR/gemini/
+cp $KERNEL_DIR/drivers/video/backlight/generic_bl.ko $ANYKERNEL_DIR/gemini/
+cd $ANYKERNEL_DIR/gemini
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
       ;;
   esac
